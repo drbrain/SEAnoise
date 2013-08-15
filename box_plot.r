5f7260f7721e48f39f545ea972a8c1490ffd9084
Eric Hodel <drbrain@segment7.net>
2013-08-14 18:57:35 -0700

    Add box plot generator for station 9

---
+++ box_plot.r
@@ -0,0 +1,30 @@
+require(graphics)
+library("DBI")
+library("RPostgreSQL")
+
+drv <- dbDriver("PostgreSQL")
+con <- dbConnect(drv, dbname="noise", user="drbrain", host="localhost")
+
+query <- function(con, query) {
+  rs <- dbSendQuery(con, query)
+
+  data <- fetch(rs, n = -1)
+
+  dbClearResult(rs)
+
+  data
+}
+
+select_dates <- query(con, "
+SELECT date_trunc('day', recorded_at)::date as \"date\", dBs
+FROM noise_entries
+WHERE station_id = 9
+ORDER BY recorded_at")
+
+png(filename="station_9.png",
+    height=800, width=1000, bg="white")
+
+boxplot(select_dates[,2] ~ select_dates[,1], select_dates, horizontal=TRUE, xlab="dB", main="Station 9")
+
+dev.off()
+
